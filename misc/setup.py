f11c18c0b0614e9c9c34410199e931aed005023c
Yongyao Jiang <yjiang@esri.com>
2019-04-11 14:39:41 -0700

    Added cleanup script for online

--- misc/setup.py
+++ misc/setup.py
@@ -2,6 +2,7 @@ from _common import *
 
 print("-*-*-*-*-*-*-*-*-*-*-*Setup begins*-*-*-*-*-*-*-*-*-*-*-*-*-")
 
-delete_for_users()
+delete_for_users(gis_online, ignore_accounts_online, target_accounts_online)
+delete_for_users(gis_playground, ignore_accounts_playground, target_accounts_playground)
 
 print("-*-*-*-*-*-*-*-*-*-*-*Setup ends*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
