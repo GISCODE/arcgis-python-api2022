813892f90d2acda648050fcbc918b0292aa695e3
Aaron Pulver <apulver@esri.com>
2019-05-02 15:11:30 -0400

    clean up tracking first

--- misc/setup.py
+++ misc/setup.py
@@ -2,8 +2,8 @@ from _common import *
 
 print("-*-*-*-*-*-*-*-*-*-*-*Setup begins*-*-*-*-*-*-*-*-*-*-*-*-*-")
 
-delete_for_users()
 clean_up_location_tracking()
+delete_for_users()
 setup_tracker_user()
 
 print("-*-*-*-*-*-*-*-*-*-*-*Setup ends*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
--- misc/teardown.py
+++ misc/teardown.py
@@ -2,7 +2,7 @@ from _common import *
 
 print("-*-*-*-*-*-*-*-*-*-*-*Teardown begins*-*-*-*-*-*-*-*-*-*-*-*-")
 
-delete_for_users()
 clean_up_location_tracking()
+delete_for_users()
 
 print("-*-*-*-*-*-*-*-*-*-*-*Teardown ends*-*-*-*-*-*-*-*-*-*-*-*-*-")
